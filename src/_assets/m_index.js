b338e8620ef2258b72be2d23b212cab976b5b5f2
jungsun.yoo <alskd82@gmail.com>
2022-05-18 17:51:39 +0900

    5월18일

--- src/_assets/m_index.js
+++ src/_assets/m_index.js
@@ -11,13 +11,21 @@ gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, ScrollSmoother, SplitText);
 import BezierEasing from "./js/class/BezierEasing.js";
 import { scrollIntoView, ease } from "./js/utils.js";
 
+import path_graph from "./global_graph_mo.json";
+
+import Swiper from 'swiper';
 
 //===============================================================================================================================
 /*===== 페이지 첫 실행  ======================*/
 //===============================================================================================================================
 
 document.addEventListener('DOMContentLoaded' , e=>{
-    Navi.init()
+    Navi.init();
+
+    HomeGrowth.init()
+    HomeGrowth.st()
+
+    homeHistory.init()
 })
 
 
@@ -32,7 +40,6 @@ document.addEventListener('DOMContentLoaded' , e=>{
 //===============================================================================================================================
 
 
-
 //===============================================================================================================================
 /*===== 네비게이션  ======================*/
 //===============================================================================================================================
@@ -42,6 +49,7 @@ const Navi = (function(exports){
 
     const addEvent=()=>{
         btMenu.addEventListener('click', e=>{
+            gsap.set( navWrap, {height: window.innerHeight });
             navWrap.classList.contains('is-active') ? navWrap.classList.remove('is-active') : navWrap.classList.add('is-active')
         });
         
@@ -55,3 +63,68 @@ const Navi = (function(exports){
     return exports;
 })({});
 
+//===============================================================================================================================
+/*===== HOME : Growth  ======================*/
+//===============================================================================================================================
+
+const HomeGrowth = (function(exports){
+    let sectionGrowth; 
+    let lottieArea
+    let graph;
+
+    const lottie_Fn=(ele)=>{
+        return lottie.loadAnimation({ 
+            container: ele,  loop: false,  autoplay: false, 
+            animationData: path_graph,
+        })
+    };
+
+    const st =()=> {
+        if(!sectionGrowth) return;
+
+        ScrollTrigger.create({
+            // markers: true, id: "graph",
+            trigger: lottieArea,
+            start: "50% 80%",
+            onEnter: ()=> graph.play(),
+        });
+    }
+
+    const init =()=>{
+        sectionGrowth = document.querySelector('.m_section-growth');
+        if(!sectionGrowth) return;
+        
+        lottieArea = document.querySelector('.m_growth_lottie-area');
+        graph = lottie_Fn(lottieArea);
+    };
+
+    exports.st = st;
+    exports.init = init;
+    return exports;
+})({});
+
+//===============================================================================================================================
+/*===== HOME : History  ======================*/
+//===============================================================================================================================
+const homeHistory = (function(exports){
+    let sectionHistory;
+    let swiper;
+
+    const init=()=>{
+        sectionHistory = document.querySelector('.m_section-history');
+        if(!sectionHistory) return;
+        swiper = new Swiper('.swiper', { spaceBetween: 8 });
+
+        sectionHistory.querySelectorAll(".m_history-slider_item").forEach((ele,i)=>{
+            ele.dataset.num = i;
+        })
+    }
+
+    exports.init = init;
+    return exports;
+})({})
+
+
+//===============================================================================================================================
+/*===== HOME :o  ======================*/
+//===============================================================================================================================
